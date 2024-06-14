e914e02a53bbcaac81d5d60320b9a950549ebb2b
szawrowski <alexander.szawrowski@gmail.com>
2024-06-15 01:57:54 +0300

    feat: Add UTF-8 string class

---
+++ include/unicore/unicore.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2024, Alexander Szawrowski
+//
+// This file is distributed under the MIT License.
+// See LICENSE file for details.
+
+#ifndef UNICORE_UNICORE_H_
+#define UNICORE_UNICORE_H_
+
+#include "unicore/types/u8string.h"
+
+#endif  // UNICORE_UNICORE_H_
