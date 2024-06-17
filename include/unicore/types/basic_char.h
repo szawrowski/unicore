eb6fb4ea1c3d39871d46c0db3f3779852f14c844
szawrowski <alexander.szawrowski@gmail.com>
2024-06-17 09:39:23 +0300

    feat: Add basic char aliases

---
+++ include/unicore/types/basic_char.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2024, Alexander Szawrowski
+//
+// This file is distributed under the MIT License.
+// See LICENSE file for details.
+
+#ifndef UNICORE_TYPES_BASIC_CHAR_H_
+#define UNICORE_TYPES_BASIC_CHAR_H_
+
+namespace unicore {
+
+using Utf8Char = char;
+using Utf16Char = char16_t;
+using Utf32Char = char32_t;
+
+}  // namespace unicore
+
+#endif  // UNICORE_TYPES_BASIC_CHAR_H_
