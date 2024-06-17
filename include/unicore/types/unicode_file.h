54401dbc84e61a496026feadc39e76326f324258
szawrowski <alexander.szawrowski@gmail.com>
2024-06-17 15:03:20 +0300

    feat: Add unicode file template

---
+++ include/unicore/types/unicode_file.h
@@ -0,0 +1,16 @@
+// Copyright (c) 2024, Alexander Szawrowski
+//
+// This file is distributed under the MIT License.
+// See LICENSE file for details.
+
+#ifndef UNICORE_TYPES_UNICODE_FILE_H_
+#define UNICORE_TYPES_UNICODE_FILE_H_
+
+namespace unicore {
+
+template <typename CharType>
+class UnicodeFile;
+
+}  // namespace unicore
+
+#endif  // UNICORE_TYPES_UNICODE_FILE_H_
