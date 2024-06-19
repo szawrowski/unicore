fb460ca5791456cb9b86eae87c7d1974220aba17
szawrowski <alexander.szawrowski@gmail.com>
2024-06-19 10:33:56 +0300

    fix: Update include paths

--- include/unicore/unicore.h
+++ include/unicore/unicore.h
@@ -6,12 +6,13 @@
 #ifndef UNICORE_UNICORE_H_
 #define UNICORE_UNICORE_H_
 
-#include "unicore/types/utf8_file.h"
-#include "unicore/types/utf8_string.h"
-
-#include "unicore/operators/utf8_char_comparison_operator.h"
-#include "unicore/operators/utf8_string_comparison_operator.h"
-#include "unicore/operators/utf8_char_stream_operator.h"
-#include "unicore/operators/utf8_string_stream_operator.h"
+#include "unicore/operators/char_comparison_operator.h"
+#include "unicore/operators/char_stream_operator.h"
+#include "unicore/operators/string_comparison_operator.h"
+#include "unicore/operators/string_stream_operator.h"
+#include "unicore/types/file/utf8_file.h"
+#include "unicore/types/string/utf16_string.h"
+#include "unicore/types/string/utf32_string.h"
+#include "unicore/types/string/utf8_string.h"
 
 #endif  // UNICORE_UNICORE_H_
